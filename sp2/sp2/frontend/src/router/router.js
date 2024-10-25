0280c245da51b9fb531ee3420dd3442952ffa6ee
MonicaPar <68394605+MonicaPar@users.noreply.github.com>
2024-10-25 15:46:32 -0600

    Update router.js

--- sp2/sp2/frontend/src/router/router.js
+++ sp2/sp2/frontend/src/router/router.js
@@ -3,32 +3,38 @@ import ReservationPlane from '../components/ReservationPlane.vue';
 import ReservationForm from '../components/ReservationForm.vue'; 
 import FotoForm from '../components/PanoramaViewer.vue'; 
 import ReservationDetails from '../components/ReservationDetails.vue';
-import DescripcionPlatillo from '../components/DescripcionPlatillo.vue';
+import Interaccion from '../components/ChatdeDescripcion.vue';
+import VistaPlatillo from '../components/VistaPlatillo.vue';
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: ReservationPlane, 
+    component: ReservationPlane, // Muestra ReservationPlane en la raíz
   },
   {
     path: '/reserve',
     name: 'Reserve',
-    component: ReservationForm, 
+    component: ReservationForm, // Asegúrate de que esta ruta tenga el componente correcto
   },
   {
     path: '/fotos',
     name: 'Foto',
-    component: FotoForm,
+    component: FotoForm, // Asegúrate de que esta ruta tenga el componente correcto
   },
   {
-    path: '/reservation-details/:id', 
+    path: '/descripcion',
+    name: 'Descripcion',
+    component: Interaccion, // Asegúrate de que esta ruta tenga el componente correcto
+  },
+  {
+    path: '/reservation-details/:id', // Usar :id para capturar el ID en la ruta
       component: ReservationDetails,
       name: 'reservation-details',
   },
   {
-    path: '/descripcion',
-      component: DescripcionPlatillo,
-      name: 'descripcion-platillo',
+    path: '/vistaplatillo', // Usar :id para capturar el ID en la ruta
+      component: VistaPlatillo,
+      name: 'vistaplatillo',
   },
 ];
 
